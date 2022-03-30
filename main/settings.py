3a3195b31def26bf91f85779a7b52f33883f13b9
Sinon <malovu01@mail.ru>
2022-03-31 02:32:41 +0600

    test_one

--- main/settings.py
+++ main/settings.py
@@ -22,10 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-e@#3nip86$-hs-msvug63!kwr@!$30zvr2(1+ljgtlxifq4_jd'
 
-# CORS
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
