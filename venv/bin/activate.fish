9165d6170c622f18324b4fa8125e37aefd007a66
Sinon <malovu01@mail.ru>
2022-04-02 13:02:29 +0600

    localhost 3000

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sinon_16/Desktop/authApp/venv"
+set -gx VIRTUAL_ENV "/Users/sinon_16/Desktop/Motion_progect/authApp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
